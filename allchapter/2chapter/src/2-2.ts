62f874327d1a2096bb0238ffc6a66e343a8c2a6b
lobo0819 <1490297242@qq.com>
2023-05-08 23:19:10 +0800

    typescript基础学习

---
+++ allchapter/2chapter/src/2-2.ts
@@ -0,0 +1,7 @@
+let str:string = '我是字符串类型'
+// str = 3 错误
+
+let arr:string = '我是个假的数组'
+// arr.forEach(()=>{}) //在开发期间就能检查到错误
+
+export {}
---
+++ allchapter/2chapter/src/2-4.ts
@@ -0,0 +1,21 @@
+// 类型注解
+let data:number = 3
+interface Student {
+    name: string
+    age: number
+}
+let stuObj:Student = {
+    name: '旺仔',
+    age: 23
+}
+
+// 类型推断
+let str = '字符串'
+let stuObj1 = {
+    name: '旺仔',
+    age: 23
+}
+
+// 定义学生
+
+export {}
